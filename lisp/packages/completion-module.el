9fe348f8e37379a34d754aeaf797b1fe5aa472f3
paradoxical-dev <jaredonnell21@gmail.com>
2025-08-14 19:58:48 -0500

    feat(codeium): removed codeium for overlay fork
    I removed codeium completely in favor of comingle which is a fork based on top of codeium. The key difference being that comingle works completely off of overlay text rather than capf allowing for separation of completions and separate bindings for said completions.

--- lisp/packages/completion-module.el
+++ lisp/packages/completion-module.el
@@ -12,9 +12,9 @@
 	 (org-mode . corfu-mode)
 	 (org-mode . corfu-popupinfo-mode))
   :config
-  ;; (setq corfu-auto t
-  ;; 	corfu-quit-no-match 'separator)
-  (setq corfu-quit-no-match 'separator)
+  (setq corfu-auto t
+	corfu-quit-no-match 'separator)
+  ;; (setq corfu-quit-no-match 'separator)
   (setq corfu-popupinfo-delay 0.1))
 
 (use-package nerd-icons-corfu
@@ -62,8 +62,10 @@
   :init
   (add-hook 'completion-at-point-functions #'cape-dabbrev)
   (add-hook 'completion-at-point-functions #'cape-file)
-  (add-hook 'completion-at-point-functions #'cape-keyword)
-  (add-hook 'completion-at-point-functions #'codeium-completion-at-point))
+  (add-hook 'completion-at-point-functions #'cape-keyword))
+  ;; (add-hook 'completion-at-point-functions #'codeium-completion-at-point))
+
+;; ai-completions ;;
 
 ;; WARN: this will have to be user made.
 ;; Create the secrets.el file in the root of your config directory and add:
@@ -72,41 +74,85 @@
   (when (file-exists-p secrets-file)
     (load secrets-file)))
 
-;; ai-completions
-(use-package codeium
-  :straight (:host github :repo "Exafunction/codeium.el")
-  :config
-  (setq codeium/metadata/api_key codeium-api-key))
-
-(use-package corfu-candidate-overlay
-  :after (corfu)
+(defvar my/comingle-is-enabled t)
+(use-package comingle
+  :straight '(:type git :host github :repo "jeff-phil/comingle.el")
+  :diminish "🧠"
+  :commands (my/comingle-toggle comingle-mode)
+  :bind
+  (:map evil-normal-state-map
+	("<leader>cc" . my/comingle-toggle)
+	("<leader>co" . comingle-chat-open))
+  (:map evil-insert-state-map
+	("M-TAB" . comingle-accept-completion)
+	("C-<return>" . comingle-accept-completion-line)
+	("C-j" . comingle-next-completion)
+	("C-k" . commingle-next-completion))
+  :hook (prog-mode . my/try-run-comingle-mode)
   :init
-  (setq corfu-auto nil)
+  ;; optionally set a timer, which might speed up things as the
+  ;; comingle local language server takes ~0.2s to start up
+  (when (bound-and-true-p my/comingle-is-enabled)
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                (run-with-timer
+                 0.1
+                 nil
+                 (lambda ()
+                   (when (comingle-state-proc comingle-state)
+                     ;; temporarily disable comingle, so that we can init
+                     (let ((my/comingle-is-enabled nil))
+                       (my/comingle-toggle))))))))
   :config
-  (keymap-global-set "M-TAB" #'corfu-candidate-overlay-complete-at-point)
-  (unless (daemonp)
-    (let ((inhibit-message t)
-          (message-log-max nil))
-      (corfu-candidate-overlay-mode +1))))
+  (defun my/try-run-comingle-mode ()
+    "Use in hook, like prog-mode-hook, so that can test first if comingle is enabled."
+    (when (bound-and-true-p my/comingle-is-enabled)
+      (comingle-mode)))
 
-;; (use-package supermaven
-;;   :straight (:host github :repo "crazywolf132/supermaven.el")
-;;   :ensure t
-;;   :config
-;;   (setq supermaven-log-level 'debug)
-;;   (supermaven-mode)
-;;   (setq supermaven-keymaps
-;; 	'((accept-suggestion . "C-<return>")
-;; 	  (clear-suggestion . "C-c C-c")
-;; 	  (accept-word . "M-<right>"))))
-;; :hook ((prog-mode . supermaven-mode)
-;;        (supermaven-mode . supermaven-setup)) ; crucial!
-;; :init
-;; (setq supermaven-keymaps
-;;       '((accept-suggestion . "C-g")
-;;         (clear-suggestion . "C-]")
-;;         (accept-word . "C-j")))
-;; (setq supermaven-ignore-filetypes '("org" "md" "txt"))
-;; (setq supermaven-log-level 'debug))
+  (defun my/comingle-toggle (&optional state)
+    "Toggle comingle's enabled state."
+    (interactive)
+    (let ((current-state (or state comingle-state)))
+      (cond
+       ;; First condition: Is comingle currently enabled?
+       (my/comingle-is-enabled
+         (when (and current-state (comingle-state-proc current-state))
+           ;; -> Then, disable it. No `progn` needed.
+           (comingle-reset))
+        (setq my/comingle-is-enabled nil)
+        (message "Comingle disabled"))
+       ;; if you don't want to use customize to save the api-key
+       (t (setopt comingle/metadata/api_key codeium-api-key)
+          ;; Reset only if it was already in a valid (but not running) state
+          (when current-state
+            (comingle-reset))
+          (comingle-init)
+          (setq my/comingle-is-enabled t)
+          (message "Comingle enabled")))))
+
+  (setq use-dialog-box nil) ;; do not use popup boxes
+
+  ;; get comingle status in the modeline
+  (setq comingle-mode-line-enable
+        (lambda (api)
+          (when (bound-and-true-p my/comingle-is-enabled)
+            (not (memq api '(CancelRequest Heartbeat AcceptCompletion))))))
+  (add-to-list 'mode-line-format '(:eval (car-safe comingle-mode-line)) t)
+  ;; alternatively for a more extensive mode-line
+  (add-to-list 'mode-line-format '(-50 "" comingle-mode-line) t)
+
+  ;; You can overwrite all the comingle configs!
+  ;; for example, we recommend limiting the string sent to comingle for better perf
+  (defun my/comingle/document/text ()
+    (buffer-substring-no-properties
+     (max (- (point) 3000) (point-min))
+     (min (+ (point) 1000) (point-max))))
+  ;; if you change the text, you should also change the cursor_offset
+  ;; warning: this is measured by UTF-8 encoded bytes
+  (defun my/comingle/document/cursor_offset ()
+    (comingle-utf8-byte-length
+     (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
+  (setq comingle/document/text 'my/comingle/document/text)
+  (setq comingle/document/cursor_offset 'my/comingle/document/cursor_offset))
 
 (provide 'completion-module)
