0a24c1b0aeb221a854098e05c3042055bb4194ad
paradoxical-dev <jaredonnell21@gmail.com>
2025-08-21 14:06:52 -0500

    refactor(hl-indent): removed

--- lisp/packages/editor-module.el
+++ lisp/packages/editor-module.el
@@ -3,20 +3,20 @@
 
 ;; INDENT ;;
 
-(use-package highlight-indent-guides
-  :ensure t
-  :config
-  (setq highlight-indent-guides-method 'character)
-  (setq highlight-indent-guides-auto-enabled nil)
-  (setq highlight-indent-guides-responsive 'top)
-  (setq highlight-indent-guides-delay 0)
+;; (use-package highlight-indent-guides
+;;   :ensure t
+;;   :config
+;;   (setq highlight-indent-guides-method 'character)
+;;   (setq highlight-indent-guides-auto-enabled nil)
+;;   (setq highlight-indent-guides-responsive 'top)
+;;   (setq highlight-indent-guides-delay 0)
 
-  (let ((bg (face-background 'default))
-	(fg (face-foreground 'link)))
-    (set-face-attribute 'highlight-indent-guides-top-character-face nil :background bg :foreground fg)
-    (set-face-attribute 'highlight-indent-guides-character-face nil :background bg :foreground "#303030"))
+;;   (let ((bg (face-background 'default))
+;; 	(fg (face-foreground 'link)))
+;;     (set-face-attribute 'highlight-indent-guides-top-character-face nil :background bg :foreground fg)
+;;     (set-face-attribute 'highlight-indent-guides-character-face nil :background bg :foreground "#303030"))
 
-  :hook (prog-mode . highlight-indent-guides-mode))
+;;   :hook (prog-mode . highlight-indent-guides-mode))
 
 ;; ZEN MODE ;;
 
